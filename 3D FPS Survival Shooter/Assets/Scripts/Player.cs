209e338e7301e06b22b3a097df7ed274da61abc0
Daniel Kirwan <danielkirwan07@gmail.com>
2021-09-30 14:28:42 +0100

    Locked cursor on play

--- 3D FPS Survival Shooter/Assets/Scripts/Player.cs
+++ 3D FPS Survival Shooter/Assets/Scripts/Player.cs
@@ -4,31 +4,66 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float _speed;
-    [SerializeField] private CharacterController _controller;
-    private Vector3 _playerVelocity;
-    //private bool _groundedPlayer;
-    [SerializeField] private float _jumpHeight;
-    [SerializeField] private float _gravityValue;
     private bool _doubleJump;
     private Vector3 _direction;
+    private Vector3 _playerVelocity;
+    [Header("Controller settings")]
+    [SerializeField] private float _speed;
+    [SerializeField] private CharacterController _controller;
+    [SerializeField] private float _jumpHeight;
+    [SerializeField] private float _gravityValue;
+    [Header("Camera Settings")]
+    [SerializeField] private Camera camera;
+    [SerializeField] private float _cameraSensitivity;
+    [SerializeField] private bool _invertY = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
         PlayerMovement();
+        CameraMovement();
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
     }
 
 
     private void FixedUpdate()
     {
 
+    }
+
+    private void CameraMovement()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        //mouseX camera movement
+        //rotate left and right Y-axis both the below do the same thing
+        //transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y + mouseX, transform.localEulerAngles.z);
+        Vector3 currentRotation = transform.localEulerAngles;
+        currentRotation.y += mouseX * _cameraSensitivity;
+        transform.localRotation = Quaternion.AngleAxis(currentRotation.y, Vector3.up);
+        //transform.rotation = Quaternion.AngleAxis(currentRotation.y, Vector3.up);
+
+        //mouseY camera movement
+        Vector3 currentCameraRotation = camera.gameObject.transform.localEulerAngles;
+        if (_invertY)
+        {
+            currentCameraRotation.x += mouseY;
+        }
+        else
+        {
+            currentCameraRotation.x -= mouseY;
+        }
+        camera.gameObject.transform.localRotation = Quaternion.AngleAxis(currentCameraRotation.x, Vector3.right);
     }
 
     private void PlayerMovement()
@@ -56,6 +91,8 @@ public class Player : MonoBehaviour
 
 
         _playerVelocity.y -= _gravityValue * Time.deltaTime;
+        //changes the direction from world space to local space, so that the player can move where the camera is pointing
+        _playerVelocity = transform.TransformDirection(_playerVelocity);
         _controller.Move(_playerVelocity * Time.deltaTime);
         
     }
